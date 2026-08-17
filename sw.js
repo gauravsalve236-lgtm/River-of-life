82da6c657b61f55a59e4db79b970ea15ee42baa1
Gaurav Salve <gauravsalve236@gmail.com>
2026-08-17 15:31:48 +0530

    Add Live Fellowship (Meetings) module and package app for iOS using CapacitorJS

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'river-of-life-cache-v20260817-PERFECT-STABLE';
+const CACHE_NAME = 'river-of-life-cache-v20260817-MOBILE-PERFECT';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
