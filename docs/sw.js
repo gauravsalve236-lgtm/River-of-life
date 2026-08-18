91bd1a9e8630108629b1b19dea2f04dea6abdc65
Gaurav Salve <gauravsalve236@gmail.com>
2026-08-18 16:59:38 +0530

    Version 27 - Removed Audio Icon Button from Meeting Control Bar, Tag v27 & v45

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'river-of-life-cache-v27-SHARE-HUB-FINAL';
+const CACHE_NAME = 'river-of-life-cache-v27-AUDIO-ICON-REMOVED';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
